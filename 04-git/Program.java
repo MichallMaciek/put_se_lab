44c3a4faaabb1a60d2de26bb5d1927021c54c623
Michał Maćkowiak <michal.mackowiak@student.put.poznan.pl>
2026-03-24 10:31:35 +0100

    patched done

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,9 +1,10 @@
+// That's the comment
 public class Program {
     public static void main(String[] args) {
-        System.out.println("H");
+        System.out.println("H ");
         System.out.println("Hello World!!!");
-        System.out.println("E");
+        System.out.println("E ");
         System.out.println("edit 3");
-        System.out.println("L");
+        System.out.println("L ");
     }
 }
