7a878baa5eeed4fcc1dc0069179ff826ec379da1
Michał Maćkowiak <michal.mackowiak@student.put.poznan.pl>
2026-03-24 09:24:12 +0100

    resolved conflict
    patched

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,6 +1,9 @@
 public class Program {
     public static void main(String[] args) {
+        System.out.println("H");
         System.out.println("Hello World!!!");
+        System.out.println("E");
         System.out.println("edit 3");
+        System.out.println("L");
     }
 }
